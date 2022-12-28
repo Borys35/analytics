861dc0d68dd2416dddb511d89fd13831716e419f
borys35 <boryskac10@gmail.com>
2022-12-28 15:56:52 +0100

    Style dashboard/[id]/page.tsx

--- dashboard/app/dashboard/[id]/page.tsx
+++ dashboard/app/dashboard/[id]/page.tsx
@@ -1,7 +1,22 @@
+import StatsItem from "@/ui/dashboard/property/StatsItem";
+
 const PropertyPage = async ({ params }: { params: { id: string } }) => {
   const { id } = params;
 
-  return <>Heeej {id}</>;
+  return (
+    <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-4">
+      <StatsItem
+        count="34532"
+        title="pages viewed"
+        variant="primary"
+        className="lg:col-span-2"
+      />
+      <StatsItem count="34532" title="pages viewed" />
+      <StatsItem count="34532" title="pages viewed" />
+      <StatsItem count="34532" title="pages viewed" />
+      <StatsItem count="34532" title="pages viewed" variant="error" />
+    </div>
+  );
 };
 
 export default PropertyPage;
---
+++ dashboard/ui/dashboard/property/StatsItem.tsx
@@ -0,0 +1,32 @@
+import classNames from "classnames";
+import { FC } from "react";
+
+interface Props {
+  count: string;
+  title: string;
+  variant?: "default" | "primary" | "error";
+  className?: string;
+}
+
+const StatsItem: FC<Props> = ({
+  count,
+  title,
+  variant = "default",
+  className,
+}) => {
+  return (
+    <div
+      className={classNames(
+        "box px-6 py-4 flex flex-col-reverse gap-2 min-h-[240px]",
+        { "border-primary-500 bg-primary-600": variant === "primary" },
+        { "border-red-500 bg-red-600": variant === "error" },
+        className
+      )}
+    >
+      <p className="text-lg font-bold">{title}</p>
+      <p className="text-6xl text-neutral-50 font-bold">{count}</p>
+    </div>
+  );
+};
+
+export default StatsItem;
