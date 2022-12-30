99c202f4639eb37e47a8b1f6c5c58d5ca17d9cf7
borys35 <boryskac10@gmail.com>
2022-12-30 13:05:36 +0100

    Add supabase database types

---
+++ dashboard/types/supabase.ts
@@ -0,0 +1,72 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      analytics: {
+        Row: {
+          id: number
+          created_at: string | null
+          user_id: string | null
+          api_key: string | null
+          events: Json[] | null
+        }
+        Insert: {
+          id?: number
+          created_at?: string | null
+          user_id?: string | null
+          api_key?: string | null
+          events?: Json[] | null
+        }
+        Update: {
+          id?: number
+          created_at?: string | null
+          user_id?: string | null
+          api_key?: string | null
+          events?: Json[] | null
+        }
+      }
+      profiles: {
+        Row: {
+          id: string
+          updated_at: string | null
+          username: string | null
+          full_name: string | null
+          avatar_url: string | null
+          website: string | null
+        }
+        Insert: {
+          id: string
+          updated_at?: string | null
+          username?: string | null
+          full_name?: string | null
+          avatar_url?: string | null
+          website?: string | null
+        }
+        Update: {
+          id?: string
+          updated_at?: string | null
+          username?: string | null
+          full_name?: string | null
+          avatar_url?: string | null
+          website?: string | null
+        }
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+  }
+}
