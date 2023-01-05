962de825c25d81212d2cb6ff84380b1e763fa202
borys35 <boryskac10@gmail.com>
2023-01-05 23:51:25 +0100

    Create index class

--- index.ts
+++ index.ts
@@ -1,3 +1,43 @@
-export function test() {
-  console.log("print test");
+import axios from "axios";
+import { PropertyEventType } from "./dashboard/types/supabaseJson";
+
+const ENDPOINT = "http://localhost:3000/api/event";
+
+export class Trakker {
+  apiKey: string;
+
+  constructor(api_key: string) {
+    this.apiKey = api_key;
+  }
+
+  initialize() {
+    const apiCall = (e: PropertyEventType) => {
+      axios.post(ENDPOINT, {
+        api_key: this.apiKey,
+        event_type: e,
+      });
+    };
+
+    window.addEventListener("load", () => {
+      apiCall("page_view");
+    });
+
+    window.addEventListener("error", () => {
+      apiCall("error");
+    });
+
+    const allButtons = document.querySelectorAll("button");
+    allButtons.forEach((b) =>
+      b.addEventListener("click", () => {
+        apiCall("click");
+      })
+    );
+
+    const allAnchors = document.querySelectorAll("button");
+    allAnchors.forEach((b) =>
+      b.addEventListener("click", () => {
+        apiCall("link_click");
+      })
+    );
+  }
 }
