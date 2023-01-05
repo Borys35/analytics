a6406b0381b5811534b97f1bd4a843474d612b92
borys35 <boryskac10@gmail.com>
2023-01-05 21:57:56 +0100

    Add 'event' API route

--- dashboard/pages/api/event.ts
+++ dashboard/pages/api/event.ts
@@ -6,17 +6,33 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  if (req.method !== "POST")
+    return res.status(400).send(`${req.method} is not allowed.`);
+
   const { event_type, api_key } = req.body as {
     event_type: PropertyEventType;
     api_key: string;
   };
 
-  const { data } = await supabase
+  const { data: analyticsData, error: analyticsError } = await supabase
     .from("analytics")
     .select("id")
     .eq("api_key", api_key);
 
-  if (!data) return res.status(400).send(`event adding failed.`);
+  if (analyticsError) return res.status(400).send(analyticsError.message);
 
-  res.status(200).send(`${event_type} event sent!`);
+  if (!analyticsData)
+    return res.status(400).send("No analytics with given api_key found.");
+
+  const { id } = analyticsData[0];
+
+  const { data, error, status } = await supabase
+    .from("events")
+    .insert({ event_type, analytics_id: id });
+
+  if (error) return res.status(400).send(error.message);
+
+  if (!data) return res.status(400).send(`Event adding failed.`);
+
+  res.status(status).send(`${event_type} event sent!`);
 }
